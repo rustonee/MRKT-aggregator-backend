00a2bca43b63d9c6d542cb1d84f513318ce7190f
rustonee <virgospirite@gmail.com>
2024-01-31 00:08:24 +0900

    fix: update cron setting

--- app.js
+++ app.js
@@ -24,15 +24,6 @@ cron.schedule("*/15 * * * *", async function () {
   await bgController.fetchCollections();
 });
 
-global.__basedir = __dirname;
-
-app.use(cors());
-app.use(logger("dev"));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: false, limit: "50mb" }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
 // Connect to MongoDB
 mongoose
   .connect(dbConfig.connectionUrl, dbConfig.connectionOptions)

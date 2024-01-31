eaf171c114348c7f65fb6036f699a45c401b4395
rustonee <virgospirite@gmail.com>
2024-01-31 15:32:12 +0700

    expose collection data api

--- controllers/collection.controlloer.js
+++ controllers/collection.controlloer.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const Collection = require("../models/collection.model");
 
 exports.getCollections = async (req, res) => {
@@ -16,40 +17,95 @@ exports.getCollections = async (req, res) => {
     const totalCounts = await Collection.countDocuments(query);
     const totalPages = Math.ceil(totalCounts / limit);
 
-    const collections = await Collection.find(query, { _id: 0, __v: 0 })
+    let collections = await Collection.find(query, { _id: 0, __v: 0 })
       .sort(sort)
       .skip((page - 1) * limit)
       .limit(limit);
 
+    collections = await Promise.all(
+      collections.map(async collection => {
+        const allCollectionsVolume = await calculateAllCollectionsVolume();
+        const _24hPriceChange = await calculate24hVolumeChange(
+          collection.contract_address
+        );
+
+        return {
+          ...collection,
+          allCollectionsVolume,
+          _24hPriceChange
+        };
+      })
+    );
+
     res.json({
       total: totalCounts,
-      collections,
+      collections
     });
   } catch (err) {
     console.log(err);
     res.status(500).send({
-      message: err.message || "Error occurred while fetching the Collections.",
+      message: err.message || "Error occurred while fetching the Collections."
     });
     return;
   }
 };
 
 exports.getCollection = async (req, res) => {
-  const address = req.params.address.toString();
-
   try {
-    const collection = await Collection.findOne({
-      contract_address: address,
+    const address = req.params.address;
+    const api_url = process.env.API_URL;
+
+    const { data: collection } = await axios.get(`${api_url}/nfts/${address}`, {
+      params: {
+        get_tokens: "false"
+      }
     });
 
+    const allCollectionsVolume = await calculateAllCollectionsVolume();
+    const _24hPriceChange = await calculate24hVolumeChange(address);
+
     res.json({
-      collection,
+      ...collection,
+      _24hPriceChange,
+      allCollectionsVolume
     });
   } catch (err) {
     console.log(err);
     res.status(500).send({
-      message: err.message || "Error occurred while fetching the Collection.",
+      message: err.message || "Error occurred while fetching the Collection."
     });
     return;
   }
+};
+
+const calculateAllCollectionsVolume = async () => {
+  const [{ allCollectionsVolume }] = await Collection.aggregate([
+    {
+      $group: {
+        _id: null,
+        allCollectionsVolume: {
+          $sum: "$volume"
+        }
+      }
+    }
+  ]);
+
+  return allCollectionsVolume;
+};
+
+const calculate24hVolumeChange = async address => {
+  const { data: allTimeVolume } = await axios.get(`${api_url}/volume`, {
+    params: {
+      period: "all",
+      nft_address: address
+    }
+  });
+
+  const currentVolume = allTimeVolume?.pop()?.volume || 0;
+  const priviousVolume = allTimeVolume?.pop()?.volume || 0;
+
+  const _24hPriceChange =
+    ((currentVolume - priviousVolume) / currentVolume) * 100;
+
+  return _24hPriceChange;
 };
